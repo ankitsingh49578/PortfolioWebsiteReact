283654cb4f94c7379b2a9ab3fc2bb1b73bc24c54
ankitsingh49578 <ankitsingh49578@gmail.com>
2024-10-20 23:05:51 +0530

    made change

--- portfolio_website_react/src/components/Navbar.jsx
+++ portfolio_website_react/src/components/Navbar.jsx
@@ -33,7 +33,7 @@ function Navbar() {
           </span>
         </div>
 
-        <div className={`md:static absolute bg-[#212428] md:pb-0 pb-12 md:w-fit w-full md:pl-0 pl-9 -mb-10 mt-16 md:mt-0 pt-8 md:pt-0 left-0 z-[1] md:z-auto transition-all ease-in duration-500 ${isOpen ? '-top-[350px]' : 'top-3'}`}>
+        <div className={`bg-[#212428] md:w-fit w-full md:pl-0 pl-9 -mb-24 -mt-2 md:mt-0 pt-5 md:pt-0 left-0 z-100 md:z-auto transition-all ease-in duration-500 ${isOpen ? 'hidden md:block' : 'block'}`}>
           <ul className="text-[#a0a8b3] flex flex-col justify-self-center md:flex-row md:gap-10 lg:gap-12 gap-6 md:pr-8 lg:pr-16 text-[22px]">
               <div className='flex flex-col group hover:cursor-pointer transition-all'>
                 <li className="group-hover:text-[#03c0c6] font-serif"><HomeOutlinedIcon className='-translate-y-[1px] -mr-[5px]'></HomeOutlinedIcon> <a href="#home">Home</a> </li>
