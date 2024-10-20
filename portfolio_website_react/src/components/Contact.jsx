ab0eb27409739c456343bf6866d8a5f14c5b624e
ankitsingh49578 <ankitsingh49578@gmail.com>
2024-10-20 12:46:53 +0530

    portfolio website

---
+++ portfolio_website_react/src/components/Contact.jsx
@@ -0,0 +1,173 @@
+import React from "react";
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+import {ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+function Contact() {
+  const [username, setusername] = useState("");
+  const [phone_no, setphone_no] = useState("");
+  const [useremail, setuseremail] = useState("");
+  const [subject, setsubject] = useState("");
+  const [message, setmessage] = useState("");
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+
+    // your EmailJs service id, template id, and public key
+    const serviceId = "service_dfn8tj1";
+    const templateId = "template_3zrvr8c";
+    const publicKey = "Qu2qtb4S7WevQerN-";
+
+    // create a new object that contains dynamic template params
+    const templateParams = {
+      from_name: username,
+      phone_no: phone_no,
+      to_name: 'Ankit Singh',
+      subject: subject,
+      message: message,
+    };
+
+    // send the email using EmailJs. Below is the promise
+    emailjs
+      .send(serviceId, templateId, templateParams, publicKey)
+      .then((res) => {
+        console.log("Email sent successfully", res);
+        toast.success('Successfully sent!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          // transition: Bounce,
+          });
+        setusername("");
+        setphone_no("");
+        setuseremail("");
+        setsubject("");
+        setmessage("");
+      })
+      .catch((error) => {
+        console.error("An error sending email: ", error);
+        toast.error('An error occured!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+          // transition: Bounce,
+          });
+      });
+  };
+
+  return (
+    <div data-aos="fade-right" id="contact" className="min-h-[850px]">
+      <h1 className="text-[#a0a8b3] text-6xl font-semibold mb-12">
+        Contact Me
+      </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-[900px] min-h-[700px] border-2 p-7 rounded-lg border-[white] border-opacity-10 bg-[#252629]"
+      >
+        <div className="flex flex-col md:flex-row justify-between gap-8">
+          <div className="flex flex-col gap-3 w-full md:w-1/2">
+            <label
+              htmlFor="fullName"
+              className="text-[#a0a8b3] text-[16px] font-semibold"
+            >
+              FULL NAME
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              value={username}
+              onChange={(e) => setusername(e.target.value)}
+              className="text-[#a0a8b3] w-[100%] h-12 rounded-lg bg-[#191b1e] p-4 border-[#03c0c6] border-[2px] border-opacity-15 placeholder-opacity-20 placeholder-[#a0a8b3]"
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-3 w-full md:w-1/2">
+            <label
+              htmlFor="phoneNumber"
+              className="text-[#a0a8b3] text-[16px] font-semibold"
+            >
+              PHONE NUMBER
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your phone no."
+              value={phone_no}
+              onChange={(e) => setphone_no(e.target.value)}
+              className="text-[#a0a8b3] w-[100%] h-12 rounded-lg bg-[#191b1e] p-4 border-[#03c0c6] border-[2px] border-opacity-15 placeholder-opacity-20 placeholder-[#a0a8b3]"
+              required
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col mt-6 gap-3">
+          <label
+            htmlFor="email"
+            className="text-[#a0a8b3] text-[16px] font-semibold"
+          >
+            EMAIL
+          </label>
+          <input
+            type="email"
+            placeholder="Enter your email id"
+            value={useremail}
+            onChange={(e) => setuseremail(e.target.value)}
+            className="text-[#a0a8b3] w-[full] h-12 rounded-lg bg-[#191b1e] p-4 border-[#03c0c6] border-[2px] border-opacity-15 placeholder-opacity-20 placeholder-[#a0a8b3]"
+            required
+          />
+        </div>
+        <div className="flex flex-col mt-6 gap-3">
+          <label
+            htmlFor="subject"
+            className="text-[#a0a8b3] text-[16px] font-semibold"
+          >
+            SUBJECT
+          </label>
+          <input
+            type="text"
+            placeholder="Enter your subject"
+            value={subject}
+            onChange={(e) => setsubject(e.target.value)}
+            className="text-[#a0a8b3] w-[full] h-12 rounded-lg bg-[#191b1e] p-4 border-[#03c0c6] border-[2px] border-opacity-15 placeholder-opacity-20 placeholder-[#a0a8b3]"
+          />
+        </div>
+        <div className="flex flex-col mt-6 gap-4">
+          <label
+            htmlFor="message"
+            className="text-[#a0a8b3] text-[16px] font-semibold"
+          >
+            YOUR MESSAGE
+          </label>
+          <textarea
+            type="email"
+            placeholder="Enter your message here..."
+            value={message}
+            onChange={(e) => setmessage(e.target.value)}
+            className="text-[#a0a8b3] w-[full] min-h-[200px] resize-none rounded-lg bg-[#191b1e] p-4 border-[#03c0c6] border-[2px] border-opacity-15 placeholder-opacity-20 placeholder-[#a0a8b3]"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="select-none mt-6 bg-[#191b1e] border-[2px] border-[#03c0c6] w-full h-12 rounded-lg border-opacity-15 text-[#a0a8b3] font-semibold hover:-translate-y-1 transition-all duration-300"
+        >
+          SEND MESSAGE
+        </button>
+        <ToastContainer />
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ portfolio_website_react/src/components/Project.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import PasswordIcon from "@mui/icons-material/Password";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import WebIcon from "@mui/icons-material/Web";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+
+function Project() {
+  return (
+    <div
+      // data-aos="fade-right"
+      id="projects"
+      className="text-[#a0a8b3] min-h-[800px] mb-20"
+    >
+      <div>
+        <h1 data-aos="fade-right" className="text-6xl font-semibold mb-10">Projects</h1>
+        <div className="grid grid-cols-1 gap-16">
+          <div data-aos="fade-right">
+            <ProjectCard
+              logo={
+                <PasswordIcon
+                  sx={{ fontSize: "55px" }}
+                  className="text-[#03c0c6]"
+                ></PasswordIcon>
+              }
+              head="Random Password Generator"
+              cont="Created a password generator with customizable length, character types, and strength evaluation."
+              anc="https://ankitsingh49578.github.io/Password-Generator/"
+              img="./projects/passwordGenerator.png"
+            ></ProjectCard>
+          </div>
+
+          <div className="flex flex-row justify-evenly" data-aos="fade-left">
+            <ProjectCard
+              logo={
+                <SportsEsportsIcon
+                  sx={{ fontSize: "55px" }}
+                  className="text-[#03c0c6]"
+                ></SportsEsportsIcon>
+              }
+              head="Simon Says Game"
+              cont="Designed and developed a memory game to challenge users with increasingly complex sequences of colors and sounds."
+              anc="https://ankitsingh49578.github.io/Simon-Says-Game/index.html"
+              img="./projects/simonSays.png"
+            ></ProjectCard>
+          </div>
+
+          <div data-aos="fade-right">
+            <ProjectCard
+              logo={
+                <WebIcon
+                  sx={{ fontSize: "55px" }}
+                  className="text-[#03c0c6]"
+                ></WebIcon>
+              }
+              head="Razorpay UI Replica"
+              cont="Developed a clone of the Razorpay website using HTML, CSS, and Tailwind CSS as a framework."
+              anc="https://razorpay-clone-virid.vercel.app/"
+              img="./projects/razorPay.png"
+            ></ProjectCard>
+          </div>
+
+          <div className="flex flex-row justify-evenly" data-aos="fade-left">
+            <ProjectCard
+              logo={
+                <FormatListBulletedIcon
+                  sx={{ fontSize: "55px" }}
+                  className="text-[#03c0c6]"
+                ></FormatListBulletedIcon>
+              }
+              head="ToDo List"
+              cont="Manage your daily tasks with ease and style. Powered by React and Tailwind CSS for a smooth experience."
+              anc="https://to-do-list-sand-mu.vercel.app/"
+              img="./projects/toDo.png"
+            ></ProjectCard>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
