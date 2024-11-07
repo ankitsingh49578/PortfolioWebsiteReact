2a65a460177b5b83d2792ce0f4799a91fe679d35
ankitsingh49578 <ankitsingh49578@gmail.com>
2024-11-07 15:03:44 +0530

    Update CvButton.jsx

--- portfolio_website_react/src/components/CvButton.jsx
+++ portfolio_website_react/src/components/CvButton.jsx
@@ -4,7 +4,7 @@ function CvButton() {
   return (
     <div className=" hover:-translate-y-[1.5px] hover:translate-x-[1.5px] transition-all hover:cursor-pointer mt-28 text-[17px] border-2 border-[#03c0c6] p-3 w-[210px] text-center font-semibold rounded-xl text-[#494747] bg-[#03c0c6] select-none hover:text-[#e4e1e1] ">
       <a
-        href="https://drive.google.com/file/d/1q7jkPuPASPVfdh-FDinpT89cuTihYoqB/view?usp=drivesdk"
+        href="https://drive.google.com/file/d/1q7jkPuPASPVfdh-FDinpT89cuTihYoqB/view"
         target="_blank"
         rel="noopener noreferrer"
       >
