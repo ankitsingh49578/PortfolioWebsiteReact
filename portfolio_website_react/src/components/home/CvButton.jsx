9ffd3127e6f7b6238fb4b519c98c2375026f2a56
ankitsingh49578 <ankitsingh49578@gmail.com>
2024-11-27 23:03:19 +0530

    resume

--- portfolio_website_react/src/components/home/CvButton.jsx
+++ portfolio_website_react/src/components/home/CvButton.jsx
@@ -4,7 +4,7 @@ function CvButton() {
   return (
     <div className=" hover:-translate-y-[1.5px] hover:translate-x-[1.5px] transition-all hover:cursor-pointer mt-28 text-[17px] border-2 border-[#03c0c6] p-3 w-[210px] text-center font-semibold rounded-xl text-[#494747] bg-[#03c0c6] select-none hover:text-[#e4e1e1] ">
       <a
-        href="https://drive.google.com/file/d/1wiyw4Bs9tsURCGHtxmPojYfBKYfxJxpB/view?usp=drivesdk"
+        href="https://drive.google.com/file/d/1OqV5LVOzGR9UEOhX0F8A4IbPGGl8-HuE/view?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
       >
