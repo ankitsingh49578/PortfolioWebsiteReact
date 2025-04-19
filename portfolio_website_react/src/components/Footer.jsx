ea5107b1e99d60a4cc656b48b4e5c299a104e4d0
ankitsingh49578 <ankitsingh49578@gmail.com>
2025-04-19 12:00:54 +0530

    update

--- portfolio_website_react/src/components/Footer.jsx
+++ portfolio_website_react/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='-mt-8 h-16 mx-auto flex justify-center text-[#a0a8b3]'>
-        © 2024. All rights reserved by Ankit Singh
+        © 2025. Made by Ankit Singh 
     </div>
   )
 }
