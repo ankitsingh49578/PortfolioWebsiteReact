7ce35d49ff25e760b50baea5a5e564b4b2828747
ankitsingh49578 <ankitsingh49578@gmail.com>
2025-05-20 18:59:46 +0530

    Update HomeLeft component to change title from Frontend Developer to Full Stack Developer.

--- portfolio_website_react/src/components/home/HomeLeft.jsx
+++ portfolio_website_react/src/components/home/HomeLeft.jsx
@@ -11,7 +11,7 @@ import {
 
 function HomeLeft() {
   const [text] = useTypewriter({
-    words: ["Frontend Developer", "Competitive Programmer"],
+    words: ["Full Stack Developer", "Competitive Programmer"],
     loop: {},
     typeSpeed: 150,
     deleteSpeed: 80,
